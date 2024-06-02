3d87ba0cc51c6a56e5d41a7a605c798bf0dba4c3
Haris M. <malik.h075659@stu.sanjac.edu>
2024-06-02 14:29:35 -0500

    finished animations for main page + created the menu pages

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".scroll-link");
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth",
+      });
+    });
+  });
+});
